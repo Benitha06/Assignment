b8bd823594274d77713ab1740a3b490523a32eb2
Benitha06 <benithatuze15@gmail.com>
2023-04-13 06:44:40 -1000

    Workit Commit

---
+++ js/index.js
@@ -0,0 +1,75 @@
+function li(){
+    let unordered = document.getElementsByTagName("h3");
+    unordered[0].innerHTML = "01";
+    
+    unordered[1].innerHTML =  "02";
+    unordered[2].innerHTML =  "03";
+}   
+
+
+function stle(){
+    let sty = document.getElementsByTagName("h3");
+    sty[0].style.marginLeft="50px";
+    sty[0].style.backgroundColor="rgba(0, 0, 0, 0.315)"
+    sty[0].style.width="30px";
+    sty[0].style.border="thin solid rgb(3, 3, 58) "
+    sty[0].style.borderRadius="50%";
+    sty[0].style.padding="10px 0 10px 10px"
+
+    sty[1].style.marginLeft="50px";
+    sty[1].style.backgroundColor="rgba(0, 0, 0, 0.315)"
+    sty[1].style.width="30px";
+    sty[1].style.border="thin solid rgb(3, 3, 58) "
+    sty[1].style.borderRadius="50%";
+    sty[1].style.padding="10px 0 10px 10px"
+
+    sty[2].style.marginLeft="50px";
+    sty[2].style.backgroundColor="rgba(0, 0, 0, 0.315)"
+    sty[2].style.width="30px";
+    sty[2].style.border="thin solid rgb(3, 3, 58) "
+    sty[2].style.borderRadius="50%";
+    sty[2].style.padding="10px 0 10px 10px"
+
+}
+function hed(){
+    let four = document.getElementsByTagName("h4");
+    four[0].innerHTML= "Actionable Insights";
+    four[1].innerHTML= "Data-Driven Decision"; 
+    four[2].innerHTML= "Actionable Insights";
+}
+function word(){
+    let words = document.getElementsByTagName("p");
+    words[0].innerHTML = "Omptimize your products,improve customer certification, stay ahead of competition.";
+    
+    words[1].innerHTML =  "Omptimize your products,improve customer certification, stay ahead of competition.";
+    words[2].innerHTML =  "Omptimize your products,improve customer certification, stay ahead of competition.";
+}  
+function ho(){
+    document.getElementById("bt").style.color= "greenyellow";
+    document.getElementById("bt").style.backgroundColor= "rgb(3, 3, 58) ";
+    document.getElementById("bt").style.border= "thin solid greenyellow";
+}
+function hov(){
+    document.getElementById("btn").style.color= "greenyellow";
+    document.getElementById("btn").style.backgroundColor= "rgb(3, 3, 58) ";
+    document.getElementById("btn").style.border= "thin solid greenyellow";
+}
+function color(){
+    let ccol = document.getElementById("lists");
+    ccol.style.backgroundColor="rgb(226, 226, 248)";
+    ccol.style.height="600px";
+    ccol.style.marginTop="-0px";
+}
+
+function app(){
+    document.getElementById("apply").style.textDecoration="none";
+}
+
+function ni() {
+this.setAttribute("src", "./images/pexels-photo-1222271.jpeg");
+
+}
+function n() {
+    this.setAttribute("src", "./images/smiling man.jpg");
+    
+    }
